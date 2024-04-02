27c0f88226f6fa95427e52dc628820b83b5149d4
Ali Tim <ali.tim03.at@gmail.com>
2024-04-02 16:30:21 +0600

    auth update

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const {login, register, current} = require("../controllers/user");
+const {auth} = require("../middleware/auth");
 const router = express.Router();
 
 router.post('/login', login)
 router.post('/register', register)
-router.get('/register', current)
+router.get('/current', auth, current)
 
 module.exports = router;
