f5d74c8079dfe548ab34bf19eeafd58fffa74807
Ali Tim <ali.tim03.at@gmail.com>
2024-06-14 15:14:32 +0500

    update

--- client/src/app/services/api.ts
+++ client/src/app/services/api.ts
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery, retry} from "@reduxjs/toolkit/query/react";
 import {RootState} from "../store";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://185.23.236.104:5050/api/',
+    baseUrl: 'http://185.23.236.104:5050/api',
     prepareHeaders: (headers, {getState}) => {
         const token = (getState() as RootState).auth.user?.token ||
             localStorage.getItem('token');
